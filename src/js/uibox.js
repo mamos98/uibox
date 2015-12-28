3bcb7fc340c96e7dada5157cbf797d7ba8196b30
mamos98 <mamos98@gmail.com>
2015-12-28 22:39:12 +0330

    Making the syntax structure a little better

--- src/js/uibox.js
+++ src/js/uibox.js
@@ -1,42 +1,43 @@
-;(function(Sizzle) {
+;
+(function(Sizzle) {
 
-	var $S = Sizzle;
+    var $S = Sizzle;
 
-	// Tabs
-	var tabs_links = $S('.tabs-links a');
-	tabs_links.forEach(function( tl ) {
-		tl.onclick = function( e ) {
-			e.preventDefault();
+    // Tabs
+    var tabs_links = $S('.tabs-links a');
+    tabs_links.forEach(function(tl) {
+        tl.onclick = function(e) {
+            e.preventDefault();
 
-			var newTab = $S(this.getAttribute('href'))[0];
+            var newTab = $S(this.getAttribute('href'))[0];
 
-			hideAllTabs();
-			showTab(newTab);
-      activeTab(this);
-		}
-	});
+            hideAllTabs();
+            showTab(newTab);
+            activeTab(this);
+        }
+    });
 
-	function hideAllTabs() {
-		var tabs_content = $S('.tabs-content div');
-		tabs_content.forEach(function( tc ) {
-			hideTab(tc);
-		});
-	};
+    function hideAllTabs() {
+        var tabs_content = $S('.tabs-content div');
+        tabs_content.forEach(function(tc) {
+            hideTab(tc);
+        });
+    };
 
-	function showTab( tab ) {
-		tab.style.display = 'inline-block';
-	};
+    function showTab(tab) {
+        tab.style.display = 'inline-block';
+    };
 
-	function hideTab( tab ) {
-		tab.style.display = 'none';
-	};
+    function hideTab(tab) {
+        tab.style.display = 'none';
+    };
 
-  function activeTab( tab ) {
-    var tabs_li = $S('.tabs-links li');
-    tabs_li.forEach(function( li ){
-      li.removeAttribute('class');
-    }); 
-    tab.parentNode.setAttribute('class', 'active');
-  };
+    function activeTab(tab) {
+        var tabs_li = $S('.tabs-links li');
+        tabs_li.forEach(function(li) {
+            li.removeAttribute('class');
+        });
+        tab.parentNode.setAttribute('class', 'active');
+    };
 
 }(Sizzle, undefined));
